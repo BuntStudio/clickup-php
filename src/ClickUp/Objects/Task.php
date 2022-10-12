d7f78d57213b63f8d5e18b90e3d1db1ced5d1a9b
madalinaag <madalina.ghetu@gmail.com>
2022-10-12 14:08:05 +0300

    PR feedback unblock new trials cron updates

--- src/ClickUp/Objects/Task.php
+++ src/ClickUp/Objects/Task.php
@@ -189,6 +189,22 @@ class Task extends AbstractObject
     }
 
     /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url): void
+    {
+        $this->url = $url;
+    }
+
+    /**
      * @throws GuzzleException
      *
      * @return Task|null
